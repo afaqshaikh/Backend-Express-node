c0fb44ceccfea229d3d2bee068ec9ecf3591328b
afaqshaikh <zaidsmai920@hotmail.com>
2021-10-23 12:21:52 +0500

    Install dependencies

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const cors = require('cors')
+const bd = require('body-parser')
 
 const app = express()
 const port = 5000
 
+app.use(cors())
+app.use(bd.urlencoded({
+    extended : false
+}))
+app.use(bd.json())
+
+
 app.get('/',(req,res)=>{
     res.send("hello from afaque")
+})
+
+app.post('/signup',(req,res)=>{
+    console.log(req.body,"Signup request")
+    // res.send("sign up")
 })
 
 app.listen(port,()=>{
