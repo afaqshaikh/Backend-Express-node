4141a264f70225fd8748dfd2a00c25aeaf132a42
afaqshaikh <zaidsmai920@hotmail.com>
2021-10-28 12:20:55 +0500

    Connect Mongodb

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 const express = require('express')
 const cors = require('cors')
 const bd = require('body-parser')
+const authModel = require('./authSchema')
 
 const app = express()
+const mongoose = require('mongoose')
 const port = 5000
 
 app.use(cors())
 app.use(bd.urlencoded({
-    extended : false
+    extended: false
 }))
 app.use(bd.json())
 
+const db = 'mongodb+srv://afaque:afaque@cluster0.slkgc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
-app.get('/',(req,res)=>{
+mongoose.connect(db).then(()=>{
+    console.log('Connection sucessful')
+}).catch((err)=>{
+    console.log('Connection is not sucessful')
+})
+
+
+// mongoose.connection.on("connected", () => {
+//     console.log('Database Connected')
+// })
+
+// mongoose.connection.on("error", () => {
+//     console.log('Database Not Connected')
+// })
+
+
+app.get('/', (req, res) => {
     res.send("hello from afaque")
 })
 
-app.post('/signup',(req,res)=>{
-    console.log(req.body,"Signup request")
+app.post('/signup', (req, res) => {
+    console.log(req.body, "Signup request")
+    let userCreate = new authModel({
+        email:req.body.email,
+        password:req.body.password
+    })
+    userCreate.save().then((response)=>{
+        // console.log(res)
+        res.status(200).send({result:response.message})
+    }).catch((err)=>{
+        // console.log(err)
+        res.status(400).send({result:err.message})
+
+    })
     // res.send("sign up")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("server is runnig....")
 })
